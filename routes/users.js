e22bb1425de90575e83b42e254fabc207bdd30f1
Priyanshu Raj <pkrr6055224@gmail.com>
2025-07-26 12:41:22 +0530

    mongodb uri added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const passport = require("passport");
 const plm = require("passport-local-mongoose");
+require('dotenv').config();
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/iconnect");
+mongoose.connect(process.env.MONGO_URI);
+
+// mongoose.connect("mongodb://127.0.0.1:27017/iconnect");
 
 const userSchema = mongoose.Schema({
   usename: String,
