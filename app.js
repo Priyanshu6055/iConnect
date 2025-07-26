5a421a8da70a5f3df2479cff4c908b109834f95d
Priyanshu Raj <pkrr6055224@gmail.com>
2025-07-26 12:51:44 +0530

    auth changed to local session to npm install connect-mongo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-const dotenv = require('dotenv').config()
+require('dotenv').config(); // load .env
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressSession = require('express-session');
+const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,31 +14,40 @@ const passport = require('passport');
 
 var app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public')); // new added
 app.set('view engine', 'ejs');
 
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// session with MongoDB store (secure for production)
 app.use(expressSession({
+  secret: 'hey hey hey', // ✅ you can move this to .env if desired
   resave: false,
   saveUninitialized: false,
-  secret: "hey hey hey"
-}))
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    collectionName: 'sessions',
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
+}));
 
-
+// passport setup
 app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(usersRouter.serializeUser());
 passport.deserializeUser(usersRouter.deserializeUser());
 
-
+// middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -49,11 +58,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
